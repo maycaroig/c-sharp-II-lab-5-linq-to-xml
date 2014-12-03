27b7f7cff4c81c9794465445406531a1a11c53ad
maycaroig <maycaroig@gmail.com>
2014-12-03 02:25:30 -0800

    Lab 5 for C# class (for C# certificate at the UCSD).

---
+++ Lab_5/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
+using System.IO;
+
+namespace Lab4
+{
+	class Program
+	{
+		private const string FILENAME = "student.xml";
+		private const string SEP = "====================";
+		private const long RECORDS = 1500;
+
+		static void Main(string[] args)
+		{
+			// Setup Console
+			Console.WindowWidth = 100;
+			Console.WindowHeight = 50;
+			Console.BufferHeight = 9999;
+			
+			// Generate students
+			GenerateStudents();
+
+			// Open student file
+			XDocument doc = OpenStudentFile();
+
+			// Peform queries
+			FindAllGradeSchoolStudents(doc);
+
+			FindValedictorianAndSalutatorian(doc);
+
+			GpaStatisticsPerGradeLevel(doc);
+
+			// Wait for completion
+			Console.ReadLine();
+		}
+
+		private static void FindAllGradeSchoolStudents(XDocument doc)
+		{
+            var query = from student in doc.Descendants("student")
+                        let grade = (Student.GradeEnum)Enum.Parse(typeof(Student.GradeEnum),
+                            student.Element("grade").Value)
+                        where grade > Student.GradeEnum.Kindergarten && grade <= Student.GradeEnum.Sixth 
+                        orderby grade, student.Element("lastName").Value, student.Element("firstName").Value 
+                        select Student.FromXElement(student);
+
+            PrintHeader("FindAllGradeSchoolStudents");
+
+            foreach (Student s in query) 
+            { 
+                Console.WriteLine(s);
+            }
+		}
+
+		private static void FindValedictorianAndSalutatorian(XDocument doc)
+		{
+            var query = (from student in doc.Descendants("student")
+                         let grade = (Student.GradeEnum)Enum.Parse(typeof(Student.GradeEnum), 
+                            student.Element("grade").Value)
+                         let gpa = float.Parse(student.Element("gpa").Value) 
+                         where grade == Student.GradeEnum.Senior
+                         orderby gpa descending 
+                         select Student.FromXElement(student)).Take(2);
+
+            PrintHeader("FindValedictorianAndSalutatorian");
+
+            if (query.Count() == 2) 
+            {
+                Console.WriteLine("Valedictorian");
+                Console.WriteLine(query.ElementAt(0)); 
+                Console.WriteLine("Salutatorian");
+                Console.WriteLine(query.ElementAt(1));
+            }
+		}
+
+		private static void GpaStatisticsPerGradeLevel(XDocument doc)
+		{
+            var query = from student in doc.Descendants("student") 
+                        let grade = (Student.GradeEnum)Enum.Parse(typeof(Student.GradeEnum), 
+                            student.Element("grade").Value) 
+                        orderby float.Parse(student.Element("gpa").Value)
+                        group student by grade into g 
+                        orderby g.Key 
+                        select g;
+            
+            PrintHeader("GpaStatisticsPerGradeLevel", false);
+
+            foreach (var group in query)
+            { 
+                Console.WriteLine(group.Key);
+                Console.WriteLine("------------");
+
+                float min = group.Min(student => float.Parse(student.Element("gpa").Value)); 
+                float max = group.Max(student => float.Parse(student.Element("gpa").Value)); 
+                float avg = group.Average(student => float.Parse(student.Element("gpa").Value));
+                Console.WriteLine(" Students: {0,4} GPA (min/max/avg): {1:0.000} / {2:0.000} / {3:0.000}", 
+                    group.Count(), min, max, avg); 
+                Console.WriteLine(); 
+            }
+		}
+
+		/// <summary>
+		/// Prints the header for the given method
+		/// </summary>
+		/// <param name="title">Title for the header</param>
+		private static void PrintHeader(string title, bool showStudentHeader = true)
+		{
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("--- " + title + " ---");
+			Console.WriteLine();
+			if (showStudentHeader)
+			{
+				Console.WriteLine(Student.HeaderString);
+			}
+		}
+
+		#region IO Methods
+		/// <summary>
+		/// Opens the student file
+		/// </summary>
+		/// <returns></returns>
+		private static XDocument OpenStudentFile()
+		{
+			return XDocument.Load(FILENAME);
+		}
+
+		/// <summary>
+		/// Generates a new student file
+		/// </summary>
+		private static void GenerateStudents()
+		{
+			Random rnd = new Random();
+			List<Student> students = new List<Student>();
+			List<string> firstNames = new List<string>();
+			List<string> lastNames = new List<string>();
+
+			Assembly asm = Assembly.GetExecutingAssembly();
+
+			// First Names
+			using (StreamReader sr = new StreamReader(asm.GetManifestResourceStream("Lab5.FirstNames.txt")))
+			{
+				while (!sr.EndOfStream)
+				{
+					string name = sr.ReadLine().Trim().ToLower();
+					if (!string.IsNullOrEmpty(name))
+					{
+						firstNames.Add(string.Format("{0}{1}", char.ToUpper(name[0]), name.Substring(1)));
+					}
+				}
+			}
+
+			// Last Names
+			using (StreamReader sr = new StreamReader(asm.GetManifestResourceStream("Lab5.LastNames.txt")))
+			{
+				while (!sr.EndOfStream)
+				{
+					string name = sr.ReadLine().Trim().ToLower();
+					if (!string.IsNullOrEmpty(name))
+					{
+						lastNames.Add(string.Format("{0}{1}", char.ToUpper(name[0]), name.Substring(1)));
+					}
+				}
+			}
+
+			// Create list of random people
+			const long INCREMENT = 999999999L / RECORDS - 100L;
+			long id = INCREMENT;
+			for (long i = 0; i < RECORDS; i++)
+			{
+				id += INCREMENT + rnd.Next(1, 100);  // Add some randomness to IDs
+				string lastName = lastNames[rnd.Next(0, lastNames.Count)];
+				string firstName = firstNames[rnd.Next(0, firstNames.Count)];
+				DateTime oldest = DateTime.Now.Subtract(TimeSpan.FromDays(365 * 22));
+				DateTime youngest = DateTime.Now.Subtract(TimeSpan.FromDays(365 * 2));
+				double oaDate = ((double)rnd.Next((int)oldest.ToOADate(), (int)youngest.ToOADate())) + rnd.NextDouble();
+				DateTime dob = DateTime.FromOADate(oaDate);
+				float gpa = (float)Math.Round(rnd.NextDouble() * 3.0 + 1.0, 3);
+				Student student = new Student(id, Student.GetGrade(dob), lastName, firstName, dob, gpa);
+				students.Add(student);
+			}
+
+			// Save the student list to a file
+			XDocument doc = CreateXDocument(students);
+			doc.Save(FILENAME, SaveOptions.None);
+		}
+
+		private static XDocument CreateXDocument(List<Student> students)
+		{
+            XDocument doc = new XDocument(
+                new XElement("students", 
+                    students.Select((item) => new XElement("student",
+                        new XAttribute("id", item.ID),
+                        new XElement("lastName", item.LastName),
+                        new XElement("firstName", item.FirstName),
+                        new XElement("dob", item.DOB),
+                        new XElement("gpa", item.GPA),
+                        new XElement("grade", item.Grade))
+                    )
+                )
+             ); 
+            return doc;
+		}
+		#endregion IO Methods
+	}
+}
---
+++ Lab_5/Student.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+	[Serializable]
+	public class Student
+	{
+		public enum GradeEnum
+		{
+			PreSchool = -1,
+            Kindergarten = 0,
+            First = 1,
+            Second,
+            Third,
+            Fourth,
+            Fifth,
+            Sixth,
+            Seventh,
+            Eighth,
+            Freshmen = 9,
+            Sophomore = 10,
+            Junior = 11,
+            Senior = 12,
+            College = 13
+		 }
+
+		#region Fields
+
+        private long m_ID = 0;
+        private string m_LastName = string.Empty;
+        private string m_FirstName = string.Empty;
+        private DateTime m_DOB = DateTime.MinValue;
+        private float m_GPA = 0f;
+        private GradeEnum m_Grade = GradeEnum.PreSchool;
+     
+        #endregion Fields
+
+		#region Properties
+        
+        public long ID
+        {
+            get { return m_ID; }
+            set 
+            {
+                if(value >=0)
+                m_ID = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return m_LastName; }
+            set { m_LastName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return m_FirstName; }
+            set { m_FirstName = value; }
+        }
+
+        public DateTime DOB
+        {
+            get { return m_DOB; }
+            set { m_DOB = value; }
+        }
+
+        public float GPA
+        {
+            get { return m_GPA; }
+            set { m_GPA = value; }
+        }
+
+        public GradeEnum Grade
+        {
+            get { return m_Grade; }
+            set { m_Grade = value; }
+        }
+
+        public int Age
+        {
+            get { return GetAge(DOB); }
+        }
+
+		#endregion Properties
+
+		#region Constructors
+
+		public Student()
+		{
+			// No operation
+		}
+
+		public Student(long id, GradeEnum grade, string lastName, string firstName, DateTime dob, float gpa)
+		{
+            ID = id;
+            Grade = grade;
+            LastName = lastName;
+            FirstName = firstName;
+            DOB = dob;
+            GPA = gpa;
+		}
+
+		#endregion Constructors
+
+		#region Methods
+		/// <summary>
+		/// Gets the age of a person with the given birthdate
+		/// </summary>
+		/// <param name="dob">Date of birth</param>
+		/// <returns>Current age of person</returns>
+		public static int GetAge(DateTime dob)
+		{
+            DateTime now = DateTime.Now;
+            int years = now.Year - dob.Year;
+            if (now.Month < dob.Month || ((now.Month == dob.Month) && (now.Day < dob.Day)))
+                years --;
+            return years;
+		}
+
+		/// <summary>
+		/// Gets the estimated grade level of a person with the given birthdate
+		/// </summary>
+		/// <param name="dob">Date of birth</param>
+		/// <returns>Estimated grade level</returns>
+		public static GradeEnum GetGrade(DateTime dob)
+		{
+            GradeEnum grade = new GradeEnum();
+
+            int age = GetAge(dob);
+            if (age < 5)
+                grade = GradeEnum.PreSchool;
+            else if (age == 5)
+                grade = GradeEnum.Kindergarten;
+            else if (age >= 18)
+                grade = GradeEnum.College;
+            else
+            {
+                if (age == 6)
+                    grade = GradeEnum.First;
+                if (age == 7)
+                    grade = GradeEnum.Second;
+                if (age == 8)
+                    grade = GradeEnum.Third;
+                if(age == 9)
+                    grade = GradeEnum.Fourth;
+                if (age == 10)
+                    grade = GradeEnum.Fifth;
+                if (age == 11)
+                    grade = GradeEnum.Sixth;
+                if (age == 12)
+                    grade = GradeEnum.Seventh;
+                if (age == 13)
+                    grade = GradeEnum.Eighth;
+                if (age == 14)
+                    grade = GradeEnum.Freshmen;
+                if (age == 15)
+                    grade = GradeEnum.Sophomore;
+                if (age == 16)
+                    grade = GradeEnum.Junior;
+                if (age == 17)
+                    grade = GradeEnum.Senior;
+                        
+            }
+
+            return grade;
+		}
+
+		/// <summary>
+		/// Parses a Student object from the given XElement
+		/// </summary>
+		/// <param name="element">element to parse</param>
+		/// <returns>Parsed student</returns>
+		public static Student FromXElement(XElement element)
+		{
+            long id = long.Parse(element.Attribute("id").Value);
+            string lastName = element.Elements("lastName").FirstOrDefault().Value;
+            string firstName = element.Elements("firstName").FirstOrDefault().Value;
+            DateTime dob = DateTime.Parse(element.Elements("dob").FirstOrDefault().Value);
+            float gpa = float.Parse(element.Elements("gpa").FirstOrDefault().Value);
+            GradeEnum grade = (GradeEnum)Enum.Parse(typeof(GradeEnum), element.Elements("grade").FirstOrDefault().Value);
+
+            return new Student(id, grade, lastName, firstName, dob, gpa);
+
+		}
+
+		/// <summary>
+		/// Student header
+		/// </summary>
+		public static string HeaderString
+		{
+			get
+			{
+				// GRADExxxxxxx 000-00-0000 LASTNAMExxxxxxxxxxxxxxxxx FIRSTNAMExxxxxxxxxxxxxxxx 00/00/0000 0.000
+				// Grade        ID          Last Name                 First Name                Birth Date GPA
+				// =============================================================================================
+				return "Grade        ID          Last Name                 First Name                Birth Date GPA" + Environment.NewLine + new string('=', 93);
+			}
+		}
+
+		public override string ToString()
+		{
+            return string.Format("{0,-12} {1:000-00-0000} {2,-25} {3,-25} {4:MM/dd/yyyy} {5:0.000}",
+                Grade, ID, LastName, FirstName, DOB, GPA);
+		}
+
+		#endregion Methods
+	}
+}
